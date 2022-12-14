d1d742f7c5338d6f585cda45a138f8a60f5fae05
윤선용 <sunyong9059@naver.com>
2022-12-15 00:30:18 +0900

    main.js li 태그 생성 및 속성 추가

--- mini-projects/sunyong/main.js
+++ mini-projects/sunyong/main.js
@@ -1,1 +1,23 @@
-console.log('hello');
+const containar = document.querySelector(".image-containar") 
+const startButton = document.querySelector(".start-button") 
+const gameText = document.querySelector(".game-text") 
+const playTime = document.querySelector(".play-time")
+
+const tileCount = 16;
+
+let tiles = [];
+
+// function
+
+//li 태그를 만들어서 반목문을 돌리고 containar에 추가하기
+function createImageTiles() {
+    const tempArray = []
+    Array(tileCount).fill().forEach( (_, i)=> {
+        const li = document.createElement("li");
+        li.setAttribute('data-index', i);
+        li.setAttribute('draggable', 'true');
+        li.classList.add(`list${i}`);
+        tempArray.push(li)
+    })
+    return tempArray;
+}
